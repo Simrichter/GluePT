6db0a4f8943973da1784442244d6d338ddc8d7db
Simrichter <137174035+Simrichter@users.noreply.github.com>
2023-06-21 13:37:06 +0200

    Gpt.py slightly updated

--- Model/Gpt.py
+++ Model/Gpt.py
@@ -14,33 +14,36 @@ class GptModel(nn.Module):
         self.register_buffer('positional_embedding', Embeddings.get_positional_encoding(cfg))
         self.emb_to_vocab = nn.Linear(cfg.embedding_dimension, cfg.vocab_size, bias=False)
         # self.blocks = nn.ModuleList(*[Block(params) for head_id in range(params.n_blocks)])
-        self.blocks = nn.Sequential(*[Block(cfg) for head_id in range(cfg.n_blocks)])
+        self.blocks = nn.Sequential(*[Block(cfg) for _ in range(cfg.n_blocks)])
         self.norm = nn.LayerNorm(cfg.embedding_dimension)
 
         self.token_embedding.weight = self.emb_to_vocab.weight  # weight tying attempt #TODO check if this works
 
     def forward(self, x, y=None):
-        x = self.token_embedding(x) + self.positional_embedding[:,x.shape[1]-1, :]
+        x = self.token_embedding(x) + self.positional_embedding[:, x.shape[1]-1, :]
         x = self.norm(self.blocks(x))
         x = self.emb_to_vocab(x)
 
         if y is None:
             loss = None
         else:
-            B, T, C = x.shape
-            x = x.view(B * T, C)
-            targets = y.view(B * T)
-            loss = func.cross_entropy(x.view(-1, x.size(-1)), targets.view(-1), ignore_index=-1)
+            B, T, vs = x.shape
+            #x = x.view(B * T, vs)
+            #targets = y.view(B * T)
+            loss = func.cross_entropy(x.view(B*T, vs), y.view(B*T), ignore_index=-1)
         return x, loss
 
     def generate(self, x, max_length):
         for i in range(max_length):
-            logits, loss = self(x[:, -self.T:])
-            logits = logits[:, -1, :]
+            #print(Embeddings.decode(x.tolist()))
+            logits, loss = self(x.view(1, -1)[:, -self.T:])
+            logits = logits[:, -1, :]#.view(-1)
+            #print(logits.shape)
             probs = func.softmax(logits, dim=-1)
-            x_next = torch.multinomial(probs, num_samples=1)
-            # print(x_next)
-            x = torch.cat((x, x_next), dim=1)  # TODO Cropping to max context_width
+            x_next = torch.argmax(probs)#torch.multinomial(probs, num_samples=1)#
+            #print(Embeddings.decode([x_next.item()]))
+            #print(Embeddings.decode([x_next.item()]))
+            x = torch.cat((x, x_next.reshape(1)))  #TODO Cropping to max context_width (already done??)
         return x
 
 

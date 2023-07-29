ccf8adbf6c3a8a1ac7af2d5b2c89386334824252
Simon Richter <simon.richter@tu-dortmund.de>
2023-07-29 02:41:26 +0200

    Dataloader working (on CPU)

--- Data.py
+++ Data.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset as DS
 import Model.config as config
 
-context_dimension = 10  # 512  # Time
+context_dimension = 512  # Time
 embedding_dimension = 768  # 192#384  # feature Channels (Should be multiple of num_heads)
 batch_size = 16  # 64  # Batch
 num_heads = 12  # for multi-head Attention
@@ -12,36 +12,40 @@ dropout = 0.2
 device = 'cpu'
 
 ''' defines the amount of Samples per Sequence of size context_length
-(1 corresponds to no overlap between samples, 2 results in approximately half-context overlap)'''
+(1 corresponds to no overlap between samples,
+ 2 will result in approximately half-context overlap,
+ set to context_dimension for full overlap)'''
 overlap = 2
 # dataset = 'Bookcorpus/bc1.pt'
 dataset_path = 'shakespeare_tokenized.pt'
 
 
 class Dataset(DS):
-    def __init__(self, cfg):
+    def __init__(self, cfg, train, train_test_percentage):
         self.context_length = cfg.context_length
         self.stepSize = cfg.context_length // overlap
         self.data = torch.load(dataset_path, map_location=torch.device('cpu'))
+        self.data = self.data[:int(train_test_percentage * len(self.data))] if train else self.data[int(train_test_percentage * len(self.data)):]
         print(self.data.size()[0])
-        self.length = -(self.data.size()[0] // -self.stepSize)  # double minus to perform efficient ceil division
+        self.length = -((self.data.size()[0]-1) // -self.stepSize)  # double minus to perform efficient ceil division
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, i):
+        assert self.length > i >= 0
         index = min(i * self.stepSize, self.data.size()[0]-self.context_length-1)
         x, y = self.data[index:index + self.context_length], self.data[index + 1:index + self.context_length + 1]
         return x, y
 
 
-# '''
+'''
 parameters = config.params(embedding_dimension, n_heads=num_heads, n_blocks=num_blocks,
                            batchsize=batch_size, context_length=context_dimension, vocab_size=vocab_size,
                            device=device, dropout=dropout)
 ds = Dataset(parameters)
 print(ds.__len__())
-x, y = ds.__getitem__(67603)  # komisches Verhalten, wenn man um 1 reduziert
-print(x)
-print(y)
-# '''
+x, y = ds.__getitem__(660)
+#print(x)
+#print(y)
+ '''
--- train.py
+++ train.py
@@ -1,56 +1,62 @@
 import torch
 import math
+
+import Data
 import Model.Gpt as gpt
 import Model.config as config
 import Model.Embeddings as emb
 from tqdm import tqdm, trange
+from torch.utils.data import DataLoader
 
 # Model Parameters
-context_dimension = 512  # Time
-embedding_dimension = 768  # 192#384  # feature Channels (Should be multiple of num_heads)
-batch_size = 16 #64  # Batch
-num_heads = 12  # for multi-head Attention
-num_blocks = 12
+context_dimension = 128#512  # Time
+embedding_dimension = 192#768  # 192#384  # feature Channels (Should be multiple of num_heads)
+batch_size = 1  # 64  # Batch
+num_heads = 6#12  # for multi-head Attention
+num_blocks = 6#12
 vocab_size = 50257
 dropout = 0.2
 
-#Training Parameters
+# Training Parameters
 use_existing_model = False
-#model_name = 'shakespeare.pt'
-model_name = 'huge_bc_model.pt'
+model_name = 'shakespeare.pt'
+# model_name = 'huge_bc_model.pt'
+epochs = 1
+num_workers = 1 # 32
 training_iterations = 20000  # 6000
 iteration_offset = 0
 eval_iters = 50
-eval_interval = 500
+eval_interval = 10#500
 always_save_checkpoints = True
 eval_tolerance = 5e-2
-train_test_percentage = 0.8
+train_test_percentage = 0.99
 learning_rate = 1e-3  # 5e-4
 min_lr = 1e-4
 warmup_iterations = 100
-lr_decay_iters = 50000 #training_iterations
+lr_decay_iters = 50000  # training_iterations
 
 best_loss = 1e8
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.set_float32_matmul_precision('high')
 
-data = torch.load('Bookcorpus/bc1.pt').to(device)
-#data = torch.load('shakespeare_tokenized.pt').to(device)
-#print(data.shape)
+# data = torch.load('Bookcorpus/bc1.pt').to(device)
+data = torch.load('shakespeare_tokenized.pt').to(device)
+# print(data.shape)
 
 train_split = data[:int(train_test_percentage * len(data))]
 test_split = data[int(train_test_percentage * len(data)):]
-#print("data loaded")
+# print("data loaded")
 
 parameters = config.params(embedding_dimension, n_heads=num_heads, n_blocks=num_blocks,
                            batchsize=batch_size, context_length=context_dimension, vocab_size=vocab_size,
                            device=device, dropout=dropout)
 
-model = torch.compile(gpt.GptModel(parameters).to(device))
-# model = gpt.GptModel(parameters).to(device)
+# model = torch.compile(gpt.GptModel(parameters).to(device))
+model = gpt.GptModel(parameters).to(device)
 model_params = filter(lambda p: p.requires_grad, model.parameters())
-optimizer = torch.optim.AdamW(model_params, learning_rate, foreach=False, fused=True)  # TODO Test if fused is good
+# optimizer = torch.optim.AdamW(model_params, learning_rate, foreach=False, fused=True)  # TODO Test if fused is good
+optimizer = torch.optim.AdamW(model_params, learning_rate)
 loss_history = {"train": [], "test": [], "test_interval": eval_interval}
 
 if use_existing_model:
@@ -59,7 +65,7 @@ if use_existing_model:
     optimizer.load_state_dict(state['optimizer'])
     iteration_offset = state['iteration']
     loss_history = state['loss_history']
-    
+
     print("Continuing from Model at iteration", iteration_offset, ", best test loss:", loss_history['test'][-1])
 
 
@@ -77,7 +83,7 @@ def get_lr(it):
     return min_lr + coeff * (learning_rate - min_lr)
 
 
-def get_batch(test=False):
+'''def get_batch(test=False):
     if test:
         d = test_split
     else:
@@ -87,10 +93,10 @@ def get_batch(test=False):
     x = torch.stack([d[i:i + context_dimension] for i in indizes]).to(device)
     y = torch.stack([d[i + 1:i + context_dimension + 1] for i in indizes]).to(device)
 
-    return x, y
+    return x, y'''
 
 
-@torch.no_grad()
+'''@torch.no_grad()
 def estimate_loss():
     model.eval()
     losses = torch.zeros(eval_iters)
@@ -100,7 +106,7 @@ def estimate_loss():
         losses[k] = loss.item()
     test_loss = losses.mean()
     model.train()
-    return test_loss
+    return test_loss'''
 
 
 def save_state(iteration, checkpoint=True):
@@ -116,18 +122,18 @@ def save_state(iteration, checkpoint=True):
         torch.save(state_to_save, 'final_{}'.format(model_name))  # Used to save a model that may be overfitted
 
 
-def training_loop(iterations):
+'''def training_loop(iterations):
     global best_loss
 
-    #ascii=" ▖▘▝▗▚▞█", ascii="░▒█", ascii=' >='
+    # ascii=" ▖▘▝▗▚▞█", ascii="░▒█", ascii=' >='
     with trange(iteration_offset, iterations, initial=iteration_offset, total=iterations) as t:
         for steps in t:
             optimizer.zero_grad(set_to_none=True)
             x, y = get_batch()
             _, loss = model(x, y)
-            if (steps)%10==0:
+            if (steps) % 10 == 0:
                 loss_history['train'].append(loss.item())
-            if (steps+1) % eval_interval == 0:
+            if (steps + 1) % eval_interval == 0:
                 test_loss = estimate_loss()
                 loss_history['test'].append(test_loss.item())
                 # print("Iteration ", steps, " train loss: ", loss.item(), " test loss: ", test_loss.item())
@@ -135,13 +141,58 @@ def training_loop(iterations):
                 if test_loss < best_loss + eval_tolerance or always_save_checkpoints:
                     best_loss = test_loss
                     save_state(steps)
-                #else:
-                    #print("test loss got larger, no checkpoint will be saved")
+                # else:
+                # print("test loss got larger, no checkpoint will be saved")
 
             loss.backward()
             optimizer.step()
             for g in optimizer.param_groups:
-                g['lr'] = get_lr(steps)
+                g['lr'] = get_lr(steps)'''
+
+
+def training_loop2(iterations):
+    global best_loss
+
+    train_set = Data.Dataset(parameters, train=True, train_test_percentage=train_test_percentage)
+    test_set = Data.Dataset(parameters, train=False, train_test_percentage=train_test_percentage)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)  # num_workers=num_workers,
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)  # num_workers=num_workers,
+    test_loss = torch.tensor(0)
+    for i in range(epochs):
+        # ascii=" ▖▘▝▗▚▞█", ascii="░▒█", ascii=' >='
+        # with trange(iteration_offset, iterations, initial=iteration_offset, total=iterations) as t:
+        for step, batch in enumerate(progressbar := tqdm(train_loader)):
+            optimizer.zero_grad(set_to_none=True)
+            x, y = batch[0].to(device), batch[1].to(device)  # get_batch()
+            _, loss = model(x, y)
+            loss.backward()
+            optimizer.step()
+            progressbar.set_postfix({'train_loss': loss.item(), 'test_loss': test_loss.item()})
+            if step % 10 == 0:
+                loss_history['train'].append(loss.item())
+            if (step + 1) % eval_interval == 0:
+                model.eval()
+                losses = torch.zeros(test_loader.__len__())
+                with torch.set_grad_enabled(False):
+                    for k, batch in enumerate(test_loader):
+                        X, Y = batch[0].to(device), batch[1].to(device)  # get_batch(test=True)
+                        logits, loss = model(X, Y)
+                        losses[k] = loss.item()
+                    test_loss = losses.mean()
+
+                    loss_history['test'].append(test_loss.item())
+                    # print("Iteration ", steps, " train loss: ", loss.item(), " test loss: ", test_loss.item())
+                    # t.set_postfix({'train_loss': loss.item(), 'test_loss': test_loss.item()})
+                    progressbar.set_postfix({'train_loss': loss.item(), 'test_loss': test_loss.item()})
+                    if test_loss < best_loss + eval_tolerance or always_save_checkpoints:
+                        best_loss = test_loss
+                        save_state(step)
+                    else:
+                        print("test loss got larger, no checkpoint will be saved")
+                    model.train()
+
+            for g in optimizer.param_groups:
+                g['lr'] = get_lr(step)
 
 
 def param_count(module=model):
@@ -153,7 +204,7 @@ def param_count(module=model):
 start_index = 0
 
 print("Number of Parameters: ", '{0:,}'.format(param_count()))
-training_loop(training_iterations)
+training_loop2(training_iterations)
 save_state(training_iterations, checkpoint=False)
 # print(emb.decode(model.generate(torch.tensor(emb.encode(s2)).view(1,-1).to(device), 100)[0].tolist()))
 model.eval()
